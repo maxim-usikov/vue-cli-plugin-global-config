7ae8560e92fcb35216c884b5a673fc6cf38d599b
Maxim Usikov <mu.usikov@gmail.com>
2019-03-26 23:16:29 +0300

    fix: wrong import

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,4 @@
 module.exports = (api, options, rootOptions) => {
-  api.injectImports('src/main.js', `import '@/plugins/element.js'`)
+  api.injectImports('src/main.js', `import '@/plugins/vue-global-config'`)
   api.render('./template');
 };
